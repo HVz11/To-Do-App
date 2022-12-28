4b02b61583fbfd1681664fa43e663da91e986df8
Vaibhav <vs630916@gmail.com>
2022-12-28 18:50:16 +0530

    minors

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
+import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [{ task: "Walk the fish" }, { task: "Groom Chikens" }],
+      todos: [],
     };
+    this.create = this.create.bind(this);
+  }
+  create(newTodo){
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
   }
   render() {
     const todos = this.state.todos.map((todo) => {
-      return <Todo task={todo.task} />;
+      return <Todo key={todo.id} task={todo.task} />;
     });
     return (
       <div>
         <h1>Todo List!</h1>
+        <TodoForm createTodo={this.create}/>
         <ul>{todos}</ul>
       </div>
     );
